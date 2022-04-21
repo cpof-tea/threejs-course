2495eb40047f0ffc996861629f63ec3be917a2f7
Oleg Makarenko <oleg.makarenko@pm.me>
2022-04-21 13:57:48 +0400

    Complete lesson 13

--- 13-3d-text/src/script.js
+++ 13-3d-text/src/script.js
@@ -2,6 +2,8 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
 
 /**
  * Base
@@ -15,21 +17,62 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
+
 /**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const matcapTexture = textureLoader.load('/textures/matcaps/8.png')
+const matcapMaterial = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
 
-/**
- * Object
- */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
+const fontLoader = new FontLoader()
+fontLoader.load(
+	'/fonts/helvetiker_regular.typeface.json',
+	(font) => {
+		const textGeometry = new TextGeometry(
+			'Hello, friend!',
+			{
+				font,
+				size: 0.5,
+				height: 0.2,
+				curveSegments: 4,
+				bevelEnabled: true,
+				bevelThickness: 0.03,
+				bevelSize: 0.02,
+				bevelOffset: 0,
+				bevelSegments: 4,
+			}
+		)
+		textGeometry.computeBoundingBox()
+		textGeometry.center()
+
+		const textMesh = new THREE.Mesh(textGeometry, matcapMaterial)
+		scene.add(textMesh)
+	}
 )
 
-scene.add(cube)
+console.time('toruses')
+const torusGeometry = new THREE.TorusBufferGeometry(0.3, 0.2, 20, 45)
 
+for (let i = 0; i < 100; i++) {
+	const torusMesh = new THREE.Mesh(torusGeometry, matcapMaterial)
+
+	torusMesh.position.x = (Math.random() - 0.5) * 10
+	torusMesh.position.y = (Math.random() - 0.5) * 10
+	torusMesh.position.z = (Math.random() - 0.5) * 10
+	torusMesh.rotation.x = (Math.random() - 0.5) * Math.PI
+	torusMesh.rotation.y = (Math.random() - 0.5) * Math.PI
+
+	const scaleFactor = Math.random()
+
+	torusMesh.scale.set(scaleFactor, scaleFactor, scaleFactor)
+
+	scene.add(torusMesh)
+}
+
+console.timeEnd('toruses')
 /**
  * Sizes
  */
